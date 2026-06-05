864f5c1953660675546cbd71b4c055f100bce9db
benjaminstarkey <benjamin.starkey25@gmail.com>
2026-06-05 10:04:45 -0400

    feat: compare poles and tf estimates

--- experiments/model_identification.m
+++ experiments/model_identification.m
@@ -17,3 +17,12 @@ data_prbs = iddata(prbs.y(:), prbs.u(:), prbs.dt);
 step_sys = tfest(data_step, 2);
 chirp_sys = tfest(data_chirp, 2);
 prbs_sys = tfest(data_prbs, 2);
+
+%% Data Analysis, Comparisons
+step_poles = pole(step_sys)
+chirp_poles = pole(chirp_sys)
+prbs_poles = pole(prbs_sys)
+
+figure
+% compare(data_prbs, prbs_sys)
+compare(data_chirp, prbs_sys)
