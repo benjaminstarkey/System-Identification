f90cb74a4d49cfdf6e1b90f37898e978061932fb
benjaminstarkey <benjamin.starkey25@gmail.com>
2026-06-05 10:31:02 -0400

    feat: model nonlinear coulomb friction in motor

--- models/actuator_truth_model.m
+++ models/actuator_truth_model.m
@@ -6,16 +6,20 @@ function x_dot = actuator_truth_model(~, x, u)
 J = 0.012;
 b = 0.004;
 Kt = 0.1;
+Fc = 0.02;
 
 % Assign states
 theta = x(1);
 omega = x(2);
 
+% Include Nonlinear Coulomb Friction Force
+friction_coulomb = Fc*sign(omega);
+
 % Initialize x_dot
 x_dot = zeros(2,1);
 
-% State Space Model
+% State Space Model (with Nonlinear friction)
 x_dot(1) = omega;
-x_dot(2) = (Kt*u - b*omega)/J;
+x_dot(2) = (Kt*u - b*omega - friction_coulomb)/J;
 
 end
