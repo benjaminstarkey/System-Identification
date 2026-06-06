bdc538f2782cd5a9724bddcf9f3bd3595d6c8bf0
benjaminstarkey <benjamin.starkey25@gmail.com>
2026-06-06 11:22:15 -0400

    feat: nonlinear model training and validation

--- identification/model_identification.m
+++ identification/model_identification.m
@@ -1,8 +1,9 @@
 %% Use Experimental Data for Model Identification
-
+clear
+clc
 % Load data from different inputs
 load step.mat
-step = data;
+stepp = data;
 
 load chirp.mat
 chirp = data;
@@ -10,21 +11,64 @@ chirp = data;
 load prbs.mat
 prbs = data;
 
-data_step = iddata(step.y(:), step.u(:), step.dt);
-data_chirp = iddata(chirp.y(:), chirp.u(:), chirp.dt);
-data_prbs = iddata(prbs.y(:), prbs.u(:), prbs.dt);
 
-step_sys = tfest(data_step, 2);
-chirp_sys = tfest(data_chirp, 2);
-prbs_sys = tfest(data_prbs, 2);
+%% Regime Looping
+names = fieldnames(stepp);
 
-%% Data Analysis, Comparisons
-step_poles = pole(step_sys)
-chirp_poles = pole(chirp_sys)
-prbs_poles = pole(prbs_sys)
+for i = 1:length(names)
+    label = names{i};
+    step_data.(label) = iddata(stepp.(label).y(:), stepp.(label).u(:), stepp.(label).dt);
+    chirp_data.(label) = iddata(chirp.(label).y(:), chirp.(label).u(:), chirp.(label).dt);
+    prbs_data.(label) = iddata(prbs.(label).y(:), prbs.(label).u(:), prbs.(label).dt);
+end
 
-%%
+%% Select Training Dataset
+z_train = prbs_data.med;
+
+orders = 2:3; % parameter to test different orders
+
+tf_models = {};
+ss_models = {};
+i = 1;
+
+for n = orders
+
+% Generate Models from Training
+tf_models{i} = tfest(z_train, n); % nth order TF
+ss_models{i} = ssest(z_train, n); % n state ss
+
+i = i+1;
+end
+
+disp('Trained Model')
+
+%% Evaluate on Training Data and Experimental Data
+trained_models = [tf_models, ss_models];
+
 figure
-% compare(data_prbs, prbs_sys)
-% compare(data_chirp, prbs_sys)
-compare(data_step, prbs_sys)
+compare(z_train, trained_models{:});
+title('Training Data Validity Comparison')
+
+[~, fit.train] = compare(z_train, trained_models{:});
+
+
+%% Evaluate on Test Data Outside Training Set
+% Step
+z_exp_step = step_data.med;
+
+figure
+compare(z_exp_step, trained_models{:});
+title('Step Data Validity Comparison')
+
+[~, fit.exp.step] = compare(z_exp_step, trained_models{:});
+
+% Chirp
+z_exp_chirp = chirp_data.med;
+
+figure
+compare(z_exp_chirp, trained_models{:});
+title('Chirp Data Validity Comparison')
+
+[~, fit.exp.chirp] = compare(z_exp_chirp, trained_models{:});
+
+
